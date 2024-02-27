7139b6d563b11175888103886637a999e452b096
Weslley555 <131720681+Weslley555@users.noreply.github.com>
2024-02-27 19:57:29 -0300

    Update soma-vetores.c

--- soma-vetores.c
+++ soma-vetores.c
@@ -5,9 +5,12 @@ int main(){
     int vetor[5]; //tipo-do-vetor  nome-do-vetor [tamanho do vetor]
     int vetor2[5];
     int vetor3[5];
-    int i;
-
-
+    int i;  
+    /*
+    i começa em 0 e é utilizado pra determinar a posição de gravação do vetor.
+    i++ utiliza para que apos gravar um número grave na próxima posição do vetor.
+    Como o vetor é de 5 números a última posição do vetor seria 4, (i < 5) garante nn passar deste número.
+    */
     for(i=0; i < 5 ; i++){
         printf("Digite um numero pro primeiro vetor:");
         scanf("%d",&vetor[i]);
@@ -18,6 +21,8 @@ int main(){
         scanf("%d",&vetor2[i]);
     }
 
+    //Ao trabalhar com vetores utilize [i] para referenciar as posições do vetores em suas corretas 
+    // posições em sequência.
     for(i=0; i<5 ; i++){
         vetor3[i] = vetor[i] + vetor2[i];
     }
